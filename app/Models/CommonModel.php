883d9abb31195b3d9cbba2d4a2c7a17fd8ffa6de
kellyfire <kellyfire611@gmail.com>
2018-03-13 20:07:50 +0700

    Thêm data common

--- app/Models/CommonModel.php
+++ app/Models/CommonModel.php
@@ -5,13 +5,18 @@ namespace App\Models;
 class CommonModel
 {
     static $states;
-    static $instance = null ; 
+    static $instance = null;
+    static $loai_nhap_xuat = null;
 
     public function __construct()
     {
         self::$states = [
             'on'  => ['value' => 1, 'text' => __('models.common.enable'), 'color' => 'success'],
             'off' => ['value' => 0, 'text' => __('models.common.disable'), 'color' => 'danger'],
+        ];
+
+        self::$loai_nhap_xuat = [
+            '_NHAP_TON_KHO_DAU_KY_' => 45
         ];
     }
 
@@ -29,5 +34,11 @@ class CommonModel
     {
         self::getInstance();
         return self::$states;
+    }
+
+    public static function getLoaiNhapXuat()
+    {
+        self::getInstance();
+        return self::$loai_nhap_xuat;
     }
 }
