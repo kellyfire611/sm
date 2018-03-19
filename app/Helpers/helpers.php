ad8e9819c983a2adb80cda6c0be46a3409923312
kellyfire611 <kellyfire611@gmail.com>
2018-03-19 10:14:23 +0700

    Thêm debug log sql query

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,3 +1,25 @@
 <?php
+if(!function_exists('getRawSqlQuery')) {
+    function getRawSqlQuery($builder) {
+        foreach ($builder->bindings as $i => $binding) {
+            if ($binding instanceof \DateTime) {
+                $builder->bindings[ $i ] = $binding->format('\'Y-m-d H:i:s\'');
+            } else {
+                if (is_string($binding)) {
+                    $builder->bindings[ $i ] = "'$binding'";
+                }
+            }
+        }
+        // Insert bindings into query
+        $boundSql = str_replace(['%', '?'], ['%%', '%s'], $builder->sql);
+        $boundSql = vsprintf($boundSql, $builder->bindings);
 
+        Log::debug(
+            "TIME - {$query->time}ms\n" .
+            "                                   UNBOUND QUERY: {$builder->sql};\n" .
+            "                                   BOUND QUERY: $boundSql;"
+        );
 
+        return $query;
+    }
+}
